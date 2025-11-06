5592199ba30090e32c1b24eb78356387707c3ebe
shrivarshan-c <shrivarshan.c2022ece@sece.ac.in>
2025-11-06 23:25:00 +0530

    obys-studio-preloader-files

--- obys-studios/script.js
+++ obys-studios/script.js
@@ -1,8 +1,37 @@
 gsap.from(".line h1",{
 
     y:150,
-    stagger:0.3,
+    stagger:0.4,
     opacity:1,
-    duration:
-   
+    duration:1
+
 })
+
+
+var loader = document.querySelector(".preloader h5").textContent;
+
+var count=0;
+setInterval((element) => {
+    if(count<100)
+    {
+        count++;
+        document.querySelector(".preloader h5").textContent= count;
+
+        console.log(count);
+
+    }
+    else
+    {
+        count=100;
+        document.querySelector(".preloader h5").textContent= count;
+        clearInterval(count);
+
+
+    }
+
+},35);
+
+console.log(loader);
+
+
+
